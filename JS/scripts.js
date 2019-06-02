f476c37fe875e97116f4de031e88c736db0ed71c
ChrisLubin <chrisx700@gmail.com>
2019-06-02 11:50:51 -0400

    Cleanup

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,83 +1,83 @@
-$(document).ready(function() {
-  //Set html height to window height
+$(document).ready(() => {
+  'use strict';
+
+  // Set html height to window height
   $("html").css("height", $(window).height());
 
-  //Variable to keep track of index of task to delete
-  var indexToDelete;
+  // Variable to keep track of index of task to delete
+  let indexToDelete;
 
-  //Appends the tasks in local storage to the task list
+  // Appends the tasks in local storage to the task list
   CreateList();
 
-  $("#test").click(function() {
+  $("#test").click(() => {
     $("#tasks").animate({
       scrollTop: $("#list").height() - 250
     }, 400);
   });
 
-  //Makes tasks sortable
+  // Makes tasks sortable
   $("#list").sortable({
     axis: "y",
-    //Saves the new order of the task list in local storage after the order is changed
-    update: function(event, ui) {
+    // Saves the new order of the task list in local storage after the order is changed
+    update: () => {
       SaveList();
     }
   });
 
-  //Adds input (if not empty) when button to add to list is clicked
+  // Adds input (if not empty) when button to add to list is clicked
   $("#ui button").click(CheckNewItem);
 
-  //Adds input (if not empty) when 'enter' button is pressed while input box is selected
-  $("#ui input").on('keypress', function(e) {
-    var key = e.keyCode || e.which;
+  // Adds input (if not empty) when 'enter' button is pressed while input box is selected
+  $("#ui input").on('keypress', e => {
+    const key = e.keyCode || e.which;
     if (key == 13) {
       CheckNewItem();
     }
   });
 
-  //Event handler for the edit icon
+  // Event handler for the edit icon
   $("#tasks").on('click', '.edit', function() {
-    var $span = $(this).parent().children();
+    const $span = $(this).parent().children();
+    let $input;
 
-    //If statement that checks if user is already editing the selected task
+    // If statement that checks if user is already editing the selected task
     if ($span.attr("class") == "editing") {
-      var $input = $(this).parent().children().children();
-      var editedInput = $input.val().trim();
+      $input = $(this).parent().children().children();
+      const editedInput = $input.val().trim();
 
-      //If the user leaves the field blank, an error is thrown
       if (CheckEditedItem(editedInput) == 1) {
-        //Removes the input box and updates the task
+        // Removes the input box and updates the task
         $input.remove();
         $span.html(editedInput);
         $span.removeClass("editing");
         SaveList();
       } else {
-        //Throws an error
+        // The user is alerted if the field is left blank
         $input.focus();
         $input.addClass("inputError");
         $input.attr('placeholder', 'A Task Was Not Entered');
         $input.val("");
       }
     } else {
-      //Adds an input box so user can edit the task
-      var text = $span.html();
+      // Adds an input box so user can edit the task
+      const text = $span.html();
       $span.html("<input type='text'></input>");
       $span.addClass("editing");
-      var $input = $(this).parent().children().children();
-      //var huh = $span.attr("class");
-      //  window.alert(huh);
+      $input = $(this).parent().children().children();
 
-      //Transfers the text that was already there to the input box
+      // Transfers the text that was already there to the input box
       $input.val(text);
       $input.focus();
     }
   });
 
-  //Event handler for when the 'enter' button is pressed when user is editing a task
+  // Event handler for when the 'enter' button is pressed when user is editing a task
   $("#tasks ul").on('keypress', '.editing', function(e) {
-    var key = e.keyCode || e.which;
+    const key = e.keyCode || e.which;
     if (key == 13) {
-      var text = $(this).children().val().trim();
-      //If the edited text is acceptible, the task is replaced with the new text
+      const text = $(this).children().val().trim();
+      // If the edited text is acceptible, the task is replaced with the new text
       if (CheckEditedItem(text) == 1) {
         $(this).children().remove();
         $(this).html(text);
@@ -91,17 +91,17 @@ $(document).ready(function() {
     }
   });
 
-  //Shows confirm dialog box when delete icon next to task is clicked
+  // Shows confirm dialog box when delete icon next to task is clicked
   $("#tasks").on("click", ".delete", function() {
     ToggleConfirmBox();
     indexToDelete = $(this).parent().index();
-    //Adds red background on selected task item
+    // Adds red background on selected task item
     $("#list li:nth-of-type(" + (indexToDelete + 1) + ")").addClass("selectedDelete", 400);
   });
 
-  //Event handler for 'yes' button on delete dialog box
-  $("#confirmBox > button:nth-of-type(1)").click(function() {
-    //Removes the task that user wants to delete
+  // Event handler for 'yes' button on delete dialog box
+  $("#confirmBox > button:nth-of-type(1)").click(() => {
+    // Removes the task that user wants to delete
     $("#list li:nth-of-type(" + (indexToDelete + 1) + ")").fadeOut(400, function() {
       $(this).remove();
       localStorage.clear();
@@ -110,22 +110,22 @@ $(document).ready(function() {
     ToggleConfirmBox();
   });
 
-  //Event handler for 'no' button on delete dialog box
-  $("#confirmBox > button:nth-of-type(2)").click(function() {
+  // Event handler for 'no' button on delete dialog box
+  $("#confirmBox > button:nth-of-type(2)").click(() => {
     ToggleConfirmBox();
-    //Removes red background on task item
+    // Removes red background on task item
     $("#list li:nth-of-type(" + (indexToDelete + 1) + ")").removeClass("selectedDelete", 400);
   });
 
-  //Toggles between showing the confirm delete dialog box and making background clickable
+  // Toggles between showing the confirm delete dialog box and making background clickable
   function ToggleConfirmBox() {
-    var overlayDisplay = $("#overlay").css("display");
+    const overlayDisplay = $("#overlay").css("display");
 
     if (overlayDisplay == "none") {
-      //Toggles between clickable and unblickable background
+      // Toggles between clickable and unblickable background
       $("#overlay").toggle();
 
-      //Toggles between making background dim and showing confirm dialog box
+      // Toggles between making background dim and showing confirm dialog box
       $("#overlay").animate({
         opacity: ".2"
       }, 400);
@@ -136,19 +136,19 @@ $(document).ready(function() {
     } else {
       $("#overlay").animate({
         opacity: "0"
-      }, 400, function() {
-        //Toggles between clickable and unblickable background
+      }, 400, () => {
+        // Toggles between clickable and unblickable background
         $("#overlay").toggle();
       });
       $("#confirmBox").animate({
         opacity: "0"
-      }, 400, function() {
+      }, 400, () => {
         $("#confirmBox").toggle();
       });
     }
   }
 
-  //Returns 1 if user input is valid, returns 0 otherwise
+  // Returns 1 if user input is valid, returns 0 otherwise
   function CheckEditedItem(text) {
     if (text == 0) {
       return 0;
@@ -157,10 +157,10 @@ $(document).ready(function() {
     }
   }
 
-  //Adds the user's input to task list with a fade in effect
+  // Adds the user's input to task list with a fade in effect
   function FadeInTask(input) {
     $("#tasks ul").append("<li> <span>" + input + "</span> <img class='delete' src='./IMG/delete.png' /> <img class='edit' src='./IMG/edit.png' /> </li>");
-    //Scrolls to bottom of list to see the latest task
+    // Scrolls to bottom of list to see the latest task
     $("#tasks").animate({
       scrollTop: $("#list").height() - 260
     }, 400);
@@ -170,17 +170,17 @@ $(document).ready(function() {
     }, 400);
   }
 
-  //Checks to see if user's input can be added to list then acts accordingly
+  // Checks to see if user's input can be added to list then acts accordingly
   function CheckNewItem() {
-    var input = $("#ui input").val().trim();
+    const input = $("#ui input").val().trim();
 
-    //If user input is empty, error is thrown
+    // The user is alerted if the field is left blank
     if (input == "") {
       $("#ui input").addClass("inputError");
       $("#ui input").attr("placeholder", "A Task Was Not Entered");
       $("#ui input").val("");
     } else {
-      //Placeholder is changed back to defualt text, input text is cleared, and User's input is appended to task list
+      // Placeholder is changed back to defualt text, input text is cleared, and User's input is appended to task list
       $("#ui input").removeClass("inputError");
       $("#ui input").attr("placeholder", "Please Enter A Task");
       $("#ui input").val("");
@@ -189,24 +189,22 @@ $(document).ready(function() {
     }
   }
 
-  //Saves the current task list in local storage
+  // Saves the current task list in local storage
   function SaveList() {
-    var i;
-
-    //Loop that goes through current task list and saves to local storage
-    for (i = 1; i <= $("#list li").length; i++) {
+    // Loop that goes through current task list and saves to local storage
+    for (let i = 1; i <= $("#list li").length; i++) {
       localStorage.setItem(("task" + i), $("#list li:nth-of-type(" + i + ") span").html());
     }
   }
 
-  //Appends the task(s) in local storage to the page
+  // Appends the task(s) in local storage to the page
   function CreateList() {
-    var i = 1;
+    let i = 1;
 
-    //Creates list if local storage has task items in it
+    // Creates list if local storage has task items in it
     if (localStorage.length != 0) {
-      //Interval that adds a task to the list every 400 milliseconds
-      var interval = setInterval(function() {
+      // Interval that adds a task to the list every 400 milliseconds
+      const interval = setInterval(() => {
         FadeInTask(localStorage.getItem("task" + i));
 
         if (i == localStorage.length) {
